21fe0b4bdc64e5c7bcedb03f829f89d4cd9d9ca1
lucas <lucasmicco@hotmail.com>
2024-08-13 10:58:04 -0300

    Add Bind no program de consumer

--- ProductsConsumer/Program.cs
+++ ProductsConsumer/Program.cs
@@ -1,8 +1,15 @@
+using Domain.Records;
 using ProductsConsumer.Consumer;
 using Services.Interface.External.Interface;
 using Services.Service.External;
-
 var builder = Host.CreateApplicationBuilder(args);
+
+string exchange = builder.Configuration.GetRequiredSection("RabbitMq:Exchange").Value ?? throw new InvalidCastException();
+string queue = builder.Configuration.GetRequiredSection("RabbitMq:Queue").Value ?? throw new InvalidCastException();
+string routingKey = builder.Configuration.GetRequiredSection("RabbitMq:RoutingKey").Value ?? throw new InvalidCastException();
+RabbitMqInfos rabbitMqInfos = new(exchange, queue, routingKey);
+builder.Services.AddSingleton(rabbitMqInfos);
+
 builder.Services.AddHostedService<Worker>();
 builder.Services.AddScoped<IRabbitMqService, RabbitMqService>();
 
