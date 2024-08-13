12391ffd698989d12f9ab76f35fb9084c8d85a93
lucas <lucasmicco@hotmail.com>
2024-08-13 10:54:56 -0300

    Adicionado consumer message worker

--- ProductsConsumer/Consumer/Worker.cs
+++ ProductsConsumer/Consumer/Worker.cs
@@ -1,3 +1,4 @@
+using Domain.Records;
 using Services.Interface.External.Interface;
 
 namespace ProductsConsumer.Consumer
@@ -6,11 +7,13 @@ namespace ProductsConsumer.Consumer
     {
         private readonly ILogger<Worker> _logger;
         private readonly IRabbitMqService _rabbitMqService;
+        private readonly RabbitMqInfos _rabbitMqInfos;
 
-        public Worker(ILogger<Worker> logger, IRabbitMqService rabbitMqService)
+        public Worker(ILogger<Worker> logger, IRabbitMqService rabbitMqService, RabbitMqInfos rabbitMqInfos)
         {
             _logger = logger;
             _rabbitMqService = rabbitMqService;
+            _rabbitMqInfos = rabbitMqInfos;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -20,6 +23,7 @@ namespace ProductsConsumer.Consumer
                 if (_logger.IsEnabled(LogLevel.Information))
                 {
                     _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    await _rabbitMqService.ConsumeMessageAsync(_rabbitMqInfos.Queue, _rabbitMqInfos.RoutingKey, _rabbitMqInfos.Exchange);
                 }
                 await Task.Delay(1000, stoppingToken);
             }
--- Services/Interface/External/Interface/IRabbitMqService.cs
+++ Services/Interface/External/Interface/IRabbitMqService.cs
@@ -1,14 +1,8 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Services.Interface.External.Interface
+﻿namespace Services.Interface.External.Interface
 {
     public interface IRabbitMqService
     {
         Task SendMessageAsync(string message);
+        Task ConsumeMessageAsync(string queueName, string routingKey, string exchangeName);
     }
 }
